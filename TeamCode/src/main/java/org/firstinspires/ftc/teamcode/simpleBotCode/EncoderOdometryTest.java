bbffcc5f2accfb72a13dcf5597520277893f9129
Todd <contactsuperior.labs@gmail.com>
2020-12-29 14:42:18 -0500

    enable RUN_USING_ENCODER for flywheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleBotCode/EncoderOdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleBotCode/EncoderOdometryTest.java
@@ -11,23 +11,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "EncoderOdometryTest", group = "Test")
 public class EncoderOdometryTest extends LinearOpMode {
-    private HardwareSimpleBot rb = new HardwareSimpleBot();
+    private final HardwareSimpleBot rb = new HardwareSimpleBot();
 
-    private MecanumOdometry odometry = new MecanumOdometry();
+    private final MecanumOdometry odometry = new MecanumOdometry();
     @Override
     public void runOpMode() throws InterruptedException {
-        rb.init(hardwareMap,this);
+        rb.init(hardwareMap, this);
         odometry.init(hardwareMap);
-        telemetry.addData("FR Encoder",rb.FR.getCurrentPosition());
-        telemetry.addData("FL Encoder",rb.FL.getCurrentPosition());
-        telemetry.addData("BL Encoder",rb.BL.getCurrentPosition());
+        telemetry.addData("FR Encoder", rb.FR.getCurrentPosition());
+        telemetry.addData("FL Encoder", rb.FL.getCurrentPosition());
+        telemetry.addData("Flywheel Encoder", rb.flywheel.getCurrentPosition());
         telemetry.update();
-        odometry.start(rb.FR.getCurrentPosition(),rb.FL.getCurrentPosition(),rb.BL.getCurrentPosition());
-        while (opModeIsActive()){
-            odometry.update(rb.FR.getCurrentPosition(),rb.FL.getCurrentPosition(),rb.BL.getCurrentPosition());
-            telemetry.addData("FR Encoder",rb.FR.getCurrentPosition());
-            telemetry.addData("FL Encoder",rb.FL.getCurrentPosition());
-            telemetry.addData("BL Encoder",rb.BL.getCurrentPosition());
+        odometry.start(rb.FR.getCurrentPosition(), rb.FL.getCurrentPosition(), rb.BL.getCurrentPosition());
+        while (opModeIsActive()) {
+            odometry.update(rb.FR.getCurrentPosition(), rb.FL.getCurrentPosition(), rb.BL.getCurrentPosition());
+            telemetry.addData("FR Encoder", rb.FR.getCurrentPosition());
+            telemetry.addData("FL Encoder", rb.FL.getCurrentPosition());
+            telemetry.addData("Flywheel Encoder", rb.flywheel.getCurrentPosition());
             telemetry.update();
         }
     }
